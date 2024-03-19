f5301bbd11527aaad0649c0572f6dbe29ba16064
sebastianselander <sebastian.selander@gmail.com>
2024-03-19 16:12:16 +0100

    fix simple parsing and small example

--- compiler/app/Main.hs
+++ compiler/app/Main.hs
@@ -1,4 +1,14 @@
 module Main where
 
+import Brainlette.Par (pProg, myLexer)
+import System.Environment
+import System.Exit
+
 main :: IO ()
-main = putStrLn "Hello, Brainlette!"
+main = do
+    [file] <- getArgs
+    text <- readFile file
+    res <- case pProg (myLexer text) of
+        Left err -> print err >> exitFailure
+        Right res -> return res
+    print res
