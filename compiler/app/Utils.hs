6a8d89f65f4f3f3d62c36dfbad19ec5341d28ea3
Samuel Hammersberg <samuel.hammersberg@gmail.com>
2024-03-22 18:45:35 +0100

    Add for.

--- compiler/app/Utils.hs
+++ compiler/app/Utils.hs
@@ -5,4 +5,8 @@ module Utils where
 {-# WARNING TODO "TODO" #-}
 pattern TODO :: a
 pattern TODO <- _
-  where TODO = error "TODO: Not yet implemented"
+  where
+    TODO = error "TODO: Not yet implemented"
+
+for :: [a] -> (a -> b) -> [b]
+for = flip map
