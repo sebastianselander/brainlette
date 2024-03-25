4d773bbb6865589ec5a4fb408c419c5d8bea1aa8
sebastianselander <sebastian.selander@gmail.com>
2024-03-25 13:03:14 +0100

    format

--- compiler/app/TC/Types.hs
+++ compiler/app/TC/Types.hs
@@ -3,16 +3,16 @@ module TC.Types where
 import Data.String (IsString)
 
 newtype Prog = Program [TopDef]
-  deriving (Eq, Ord, Show, Read)
+    deriving (Eq, Ord, Show, Read)
 
 data TopDef = FnDef Type Ident [Arg] Blk
-  deriving (Eq, Ord, Show, Read)
+    deriving (Eq, Ord, Show, Read)
 
 data Arg = Argument Type Ident
-  deriving (Eq, Ord, Show, Read)
+    deriving (Eq, Ord, Show, Read)
 
 newtype Blk = Block [Stmt]
-  deriving (Eq, Ord, Show, Read)
+    deriving (Eq, Ord, Show, Read)
 
 data Stmt
     = Empty
@@ -27,22 +27,18 @@ data Stmt
     | CondElse Expr Stmt Stmt
     | While Expr Stmt
     | SExp Expr
-  deriving (Eq, Ord, Show, Read)
+    deriving (Eq, Ord, Show, Read)
 
 data Item = NoInit Ident | Init Ident Expr
-  deriving (Eq, Ord, Show, Read)
+    deriving (Eq, Ord, Show, Read)
 
-data Type = Int | Double | Bool | Void | Fun Type [Type]
-  deriving (Eq, Ord, Show, Read)
+data Type = Int | Double | Bool | String | Void | Fun Type [Type]
+    deriving (Eq, Ord, Show, Read)
 
 data Expr
     = EVar Type Ident
-    | ELitInt Integer
-    | ELitDoub Double
-    | ELitTrue
-    | ELitFalse
+    | ELit Type Lit
     | EApp Type Ident [Expr]
-    | EString String
     | Neg Type Expr
     | Not Expr
     | EMul Type Expr MulOp Expr
@@ -50,16 +46,23 @@ data Expr
     | ERel Expr RelOp Expr
     | EAnd Expr Expr
     | EOr Expr Expr
-  deriving (Eq, Ord, Show, Read)
+    deriving (Eq, Ord, Show, Read)
+
+data Lit
+    = LitInt Integer
+    | LitDouble Double
+    | LitBool Bool
+    | LitString String
+    deriving (Eq, Ord, Show, Read)
 
 data AddOp = Plus | Minus
-  deriving (Eq, Ord, Show, Read)
+    deriving (Eq, Ord, Show, Read)
 
 data MulOp = Times | Div | Mod
-  deriving (Eq, Ord, Show, Read)
+    deriving (Eq, Ord, Show, Read)
 
 data RelOp = LTH | LE | GTH | GE | EQU | NE
-  deriving (Eq, Ord, Show, Read)
+    deriving (Eq, Ord, Show, Read)
 
 newtype Ident = Ident String
-  deriving (Eq, Ord, Show, Read, IsString)
+    deriving (Eq, Ord, Show, Read, IsString)
