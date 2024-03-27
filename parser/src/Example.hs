8883d4657c930bb3ad89f1ddec7bba8290f33c5b
sebastianselander <sebastian.selander@gmail.com>
2024-03-27 13:50:07 +0100

    add one more example

--- parser/src/Example.hs
+++ parser/src/Example.hs
@@ -6,6 +6,12 @@ import Data.Text.IO (readFile)
 
 example1 :: IO ()
 example1 = do
-    let file = "inputs/prg.bl"
+    let file = "inputs/example1.bl"
+    inp <- readFile file
+    print $ program file inp
+
+example2 :: IO ()
+example2 = do
+    let file = "inputs/example2.bl"
     inp <- readFile file
     print $ program file inp
