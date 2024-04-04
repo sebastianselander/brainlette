b71df9d3b4e9fdc5fa0ea2b5e95f9083a8ebe337
sebastianselander <sebastian.selander@gmail.com>
2024-04-04 13:55:28 +0200

    adopt ePutStrLn

--- compiler/app/Main.hs
+++ compiler/app/Main.hs
@@ -8,11 +8,9 @@ import BrainletteParser
 import BranchReturns (check)
 import Control.Monad (unless)
 import Data.Text (pack, unpack)
-import Data.Text.IO (hPutStrLn)
 import System.Directory (doesFileExist)
 import System.Environment
 import System.Exit
-import System.IO (stderr)
 import TypeChecker.Tc (tc)
 import Utils (ePrint, ePutStrLn)
 
@@ -20,10 +18,10 @@ main :: IO ()
 main = do
     args <- getArgs
     file <- case args of
-        [] -> hPutStrLn stderr "brainlette: no input file" >> exitFailure
+        [] -> ePutStrLn "brainlette: no input file" >> exitFailure
         (file : _) -> do
             b <- doesFileExist file
-            unless b $ hPutStrLn stderr "brainlette: file does not exist" >> exitFailure
+            unless b $ ePutStrLn "brainlette: file does not exist" >> exitFailure
             return file
     text <- readFile file
     res <- case program file (pack text) of
@@ -34,28 +32,28 @@ main = do
     ePrint res
 
     res <- case check res of
-        Left err -> hPutStrLn stderr err *> exitFailure
+        Left err -> ePutStrLn err *> exitFailure
         Right res -> return res
 
     ePutStrLn "\n--- Check output ---"
     ePrint res
 
     res <- case tc res of
-        Left err -> hPutStrLn stderr err *> exitFailure
+        Left err -> ePutStrLn err *> exitFailure
         Right res -> return res
 
     ePutStrLn "\n--- Typecheck output ---"
     ePrint res
 
     res <- case bmm res of
-        Left err -> hPutStrLn stderr err *> exitFailure
+        Left err -> ePutStrLn err *> exitFailure
         Right res -> return res
 
     ePutStrLn "\n--- BMM output ---"
     ePrint res
 
     res <- case braingen res of
-        Left err -> hPutStrLn stderr err *> exitFailure
+        Left err -> ePutStrLn err *> exitFailure
         Right res -> return res
 
     ePutStrLn "\n--- LLVM IR output ---"
