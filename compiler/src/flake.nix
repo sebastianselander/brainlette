6646feec44094b1a53dc3e4b075f739049a6ea46
sebastianselander <sebastian.selander@gmail.com>
2024-05-26 22:45:54 +0200

    remove unused flake deps

--- compiler/src/flake.nix
+++ compiler/src/flake.nix
@@ -23,15 +23,11 @@
         devShells = {
           default = pkgs.mkShell {
             nativeBuildInputs = with pkgs; [
-              just
               ghc
               haskell-language-server
               haskellPackages.cabal-install
               haskellPackages.fourmolu
               haskellPackages.hoogle
-              haskellPackages.BNFC
-              haskellPackages.alex
-              haskellPackages.happy
               llvmPackages_latest.llvm
               lldb
               zlib
