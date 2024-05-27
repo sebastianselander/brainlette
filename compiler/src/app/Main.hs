70e8a7cae462571ad7417f0c31e4ba8962c151e5
sebastianselander <sebastian.selander@gmail.com>
2024-05-27 12:13:58 +0200

    Add options instead of CPP

--- compiler/src/app/Main.hs
+++ compiler/src/app/Main.hs
@@ -1,124 +1,145 @@
-{-# LANGUAGE CPP #-}
+{-# LANGUAGE ApplicativeDo #-}
+{-# LANGUAGE OverloadedRecordDot #-}
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE RecordWildCards #-}
 
 module Main (main) where
 
 import BMM.TcToBmm (bmm)
 import Braingen.Ir (braingen)
-import Control.Monad (unless)
+import Control.Monad (unless, when)
+import Data.Functor (void)
 import Data.Text (Text, unlines)
 import Data.Text.IO (getContents, hPutStrLn, putStrLn, readFile, writeFile)
 import Frontend.BranchReturns (branchCheck)
 import Frontend.Error (report)
-import Frontend.Parser.BrainletteParser
+import Frontend.Parser.BrainletteParser (program)
 import Frontend.Renamer (rename)
 import Frontend.Tc.Tc (tc)
 import Frontend.Uniter (importFiles)
 import Lifting.Lifter (lift)
+import Options.Applicative
 import Runtime (runtime)
 import System.Directory (doesFileExist)
-import System.Environment
-import System.Exit
+import System.Environment (getArgs)
+import System.Exit (exitFailure)
+import System.FilePath (takeBaseName)
 import System.IO (stderr)
+import System.Process (StdStream (..), createProcess, proc, std_out)
 import Utils (Pretty (..), thow)
 import Prelude hiding (getContents, putStrLn, readFile, unlines, writeFile)
 
+newtype Options = Options {submissionMode :: Bool}
+    deriving (Show)
+
+options :: Parser Options
+options = do
+    submissionMode <-
+        flag
+            True
+            False
+            ( long "normal"
+                <> short 'n'
+                <> help "print errors"
+            )
+    return Options {..}
+
 main :: IO ()
 main = do
+    options <- execParser (info options fullDesc)
     args <- getArgs
     (file, text) <- case args of
         [] -> ("stdin",) <$> getContents
         (file : _) -> do
             b <- doesFileExist file
-            unless b $ ePutStrLn "brainlette: file does not exist" >> exitFailure
+            unless b $
+                ePutStrLn
+                    options
+                    "brainlette: file does not exist"
+                    >> exitFailure
             text <- readFile file
             return (file, text)
 
-    ePutStrLn "--- Parse output ---"
+    ePutStrLn options "--- Parse output ---"
     res <- case program file text of
-        Left err -> errorExit (thow err)
+        Left err -> errorExit options (thow err)
         Right res -> return res
 
-    -- ePutStrLn "--- Files to import ---"
+    ePutStrLn options "--- Files to import ---"
     res <- importFiles res
     res <- case res of
-        Left err -> errorExit (report err)
+        Left err -> errorExit options (report err)
         Right res -> return res
-    ePutStrLn (pretty 0 res)
+    ePutStrLn options (pretty 0 res)
 
-    ePutStrLn "--- Renamer output ---"
+    ePutStrLn options "--- Renamer output ---"
     (res, _) <- case rename res of
-        Left err -> errorExit err
+        Left err -> errorExit options err
         Right res -> return res
 
-    ePutStrLn (pretty 0 res)
+    ePutStrLn options (pretty 0 res)
     res <- case branchCheck res of
-        Left err -> errorExit err
+        Left err -> errorExit options err
         Right res -> return res
 
-    ePutStrLn "\n--- Branch check output ---"
+    ePutStrLn options "\n--- Branch check output ---"
 
-    ePutStrLn (pretty 0 res)
+    ePutStrLn options (pretty 0 res)
 
     res <- case tc res of
-        Left err -> errorExit err
+        Left err -> errorExit options err
         Right res -> return res
 
-    ePutStrLn "\n--- Typecheck output ---"
-    ePutStrLn (pretty 0 res)
+    ePutStrLn options "\n--- Typecheck output ---"
+    ePutStrLn options (pretty 0 res)
 
-    ePutStrLn "\n--- Lifter output ---"
+    ePutStrLn options "\n--- Lifter output ---"
 
     (res, lifteds) <- return (lift res)
-    ePutStrLn (pretty 0 res)
+    ePutStrLn options (pretty 0 res)
 
-    ePutStrLn "\n--- BMM output ---"
+    ePutStrLn options "\n--- BMM output ---"
 
     (res, funs) <- return $ bmm res
-    ePutStrLn (pretty 0 res)
+    ePutStrLn options (pretty 0 res)
     -- writeFile "output.bmm" (pretty 0 res)
 
     res <- return $ braingen res lifteds funs
 
-    ePutStrLn "\n--- LLVM IR output ---"
+    ePutStrLn options "\n--- LLVM IR output ---"
     let res' = unlines [runtime, res]
-    ePutStrLn res'
+    ePutStrLn options res'
 
     writeFile "output.ll" res'
-#if DEBUG
-    void $
-        createProcess $
-            proc
-                "clang"
-                [ "-O3"
-                , "output.ll"
-                , "-o"
-                , takeBaseName file
-                ]
-#endif
+    if options.submissionMode
+        then pure ()
+        else
+            void $
+                createProcess $
+                    ( proc
+                        "clang"
+                        [ "-O3"
+                        , "output.ll"
+                        , "-o"
+                        , takeBaseName file
+                        ]
+                    )
+                        { std_out = UseHandle stderr
+                        }
     putStrLn res'
-    ok
+    ok options
 
 -- | Print string to stdErr
-ePutStrLn :: Text -> IO ()
-#if DEBUG
-ePutStrLn = hPutStrLn stderr
-#else
-ePutStrLn _ = pure ()
-#endif
+ePutStrLn :: Options -> Text -> IO ()
+ePutStrLn o txt = if o.submissionMode then pure () else hPutStrLn stderr txt
 
 -- | Print error and exit
-errorExit :: forall a. Text -> IO a
-#ifdef DEBUG
-errorExit err = hPutStrLn stderr err *> exitFailure
-#else
-errorExit _ = hPutStrLn stderr "ERROR" *> exitFailure
-#endif
+errorExit :: forall a. Options -> Text -> IO a
+errorExit o err =
+    if o.submissionMode
+        then hPutStrLn stderr "ERROR" *> exitFailure
+        else hPutStrLn stderr err *> exitFailure
 
 -- | Print OK
-ok :: IO ()
-#ifdef DEBUG
-ok = pure ()
-#else
-ok = hPutStrLn stderr "OK"
-#endif
+ok :: Options -> IO ()
+ok o = when o.submissionMode $ hPutStrLn stderr "OK"
